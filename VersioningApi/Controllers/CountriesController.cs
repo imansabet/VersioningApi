ec08ff0c3dd2889255007e43db0c60f7b2cf5b50
iman <dev.imansabet@gmail.com>
2024-01-18 18:07:47 +0330

    Add CountriesController Get Endpoint + Add Models (Domain and DTO)

---
+++ VersioningApi/Controllers/CountriesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VersioningApi.Models.DTOs;
+
+namespace VersioningApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            var countriesDomainModel = CountriesData.Get();
+            // map domain to dto
+            var response = new List<CountryDTO>();
+            foreach (var countryDomain in countriesDomainModel) 
+            {
+                response.Add(new CountryDTO
+                {
+                    Id = countryDomain.Id,
+                    Name = countryDomain.Name,
+                });
+            }
+            return Ok(response);
+
+        }
+    }
+}
---
+++ VersioningApi/Models/Domain/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace VersioningApi.Models.Domain
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
