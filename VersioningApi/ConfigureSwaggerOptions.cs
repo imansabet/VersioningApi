60d4860d0c7b71fa59c2535b99e5e1307f7b044e
iman <dev.imansabet@gmail.com>
2024-01-19 13:00:31 +0330

    Config Swagger for Versioning

---
+++ VersioningApi/ConfigureSwaggerOptions.cs
@@ -0,0 +1,39 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace VersioningApi
+{
+    public class ConfigureSwaggerOptions : IConfigureNamedOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider apiDescriptionProvider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider apiDescriptionProvider)
+        {
+            this.apiDescriptionProvider = apiDescriptionProvider;
+        }
+        public void Configure(string? name, SwaggerGenOptions options)
+        {
+            Configure(options);
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in apiDescriptionProvider.ApiVersionDescriptions) 
+            {
+                options.SwaggerDoc(description.GroupName, CreateVersionInfo(description));
+            }
+        }
+        private OpenApiInfo CreateVersionInfo(ApiVersionDescription description) 
+        {
+            var info = new OpenApiInfo
+            {
+                Title = "Your versioned Api",
+                Version = description.ApiVersion.ToString()
+            };
+            return info;
+        }
+    }
+}
--- VersioningApi/Program.cs
+++ VersioningApi/Program.cs
@@ -1,24 +1,48 @@
+﻿using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using VersioningApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddMvc();
 
-builder.Services.AddApiVersioning(options => {
-    options.AssumeDefaultVersionWhenUnspecified = true;
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();
+})
+.AddMvc() // ← new; brings in MVC Core support
+.AddApiExplorer(options => 
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
 
 var app = builder.Build();
-
+var versionDescriptionProvider =
+    app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        foreach(var description in versionDescriptionProvider.ApiVersionDescriptions) 
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                    description.GroupName.ToUpperInvariant()); 
+        }
+    
+    });
 }
 
 app.UseHttpsRedirection();
